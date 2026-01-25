48802007677ad39528aadee6bc50d18a87d7c1f0
Éloi Rivard <eloi@yaal.coop>
2026-01-25 16:26:56 +0100

    test: use httpx Client context manager

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,10 +8,10 @@ from scim2_client.engines.httpx import SyncSCIMClient
 
 @pytest.fixture
 def sync_client(httpserver):
-    client = Client(base_url=f"http://localhost:{httpserver.port}")
-    scim_client = SyncSCIMClient(
-        client,
-        resource_models=[User, Group],
-    )
-    scim_client.register_naive_resource_types()
-    return scim_client
+    with Client(base_url=f"http://localhost:{httpserver.port}") as client:
+        scim_client = SyncSCIMClient(
+            client,
+            resource_models=[User, Group],
+        )
+        scim_client.register_naive_resource_types()
+        yield scim_client
